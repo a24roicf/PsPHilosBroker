bc404d72cc16c41fe888ec485337ba2169743172
Sharkoise <a24roicf@iesantonlosada.gal>
2025-12-15 20:31:00 +0100

    Arreglo menor en el Frame

--- src/main/java/sharko/psphilosbroker/MainJFrame.java
+++ src/main/java/sharko/psphilosbroker/MainJFrame.java
@@ -50,10 +50,7 @@ public class MainJFrame extends javax.swing.JFrame {
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null},
-                {null, null, null},
-                {null, null, null},
-                {null, null, null}
+
             },
             new String [] {
                 "Nombre", "Saldo", "Activos"
